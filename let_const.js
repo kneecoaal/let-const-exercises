b644f3f85cb423b7157da772b8c1bf3f29091d51
Nicole Ferrucci <67844900+kneecoaal@users.noreply.github.com>
2023-10-24 18:59:50 -0700

    Add files via upload

---
+++ let_const.js
@@ -0,0 +1,39 @@
+/* 
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+*/
+
+//Write an ES2015 version
+
+const PI = 3.14;
+PI = 42;
+
+/*
+Quiz:
+
+What is the difference between var and let?
+
+>> variables declared by let 
+are only available inside the block
+ they are defined in while variables 
+ declared by var are available 
+ throughout the function in which 
+ they're declared.
+
+What is the difference between var and const?
+
+>> var variables can be updated and re-declared 
+within its scope while const varables can neither be 
+updated nor re-declared.
+
+What is the difference between let and const?
+
+>> let variables can be updated but not re-declared
+while const variables can not do either.
+
+What is hoisting?
+>> the default behavior of the movement of all 
+declarations to the top of the current scope 
+(to the top of the current script or the current
+function)
+*/
